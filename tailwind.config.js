4c75cd921e2ac18b43af7194a1e0b952553089ce
lapiito <lapeetoblaze@gmail.com>
2022-07-18 06:46:26 +0100

    Hello World!

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
                 neon: '#52DEE5',
                 text: '#C7D3FF',
                 textDark: '#A1ABBF',
-                bgblue: '#222E50',
+                bgblue: '#240090',
                 darkerBlue: '#0A192F',
             },
             fontFamily: {
